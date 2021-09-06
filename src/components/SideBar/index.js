406956a174de8ef3dfc7c71c2e28f651c105602d
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-09-06 13:44:24 -0300

    that's it

---
+++ src/components/SideBar/index.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react";
+
+import { connect } from "react-redux";
+
+function toggleLesson(module, lesson) {
+  return {
+    type: 'TOGGLE_LESSON',
+    module,
+    lesson,
+  }
+}
+
+const SideBar = ({ modules, dispatch }) => {
+  return (
+    <aside>
+      { modules.map(module => (
+        <div key={module.id}>
+          <strong>{module.title}</strong>
+          <ul>
+            { module.lessons.map(lesson => (
+              <li key={lesson.id}>
+                {lesson.title}
+                <button onClick={() => dispatch(toggleLesson(module, lesson))}>Selecionar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </aside>
+  )
+}
+
+export default connect(state => ({ modules: state.modules }))(SideBar);
